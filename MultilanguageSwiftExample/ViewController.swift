226b6df896f586cde6c3ad11b623222973d82213
Dmitry Onishchuk <dmitry.oni90@gmail.com>
2022-01-01 18:30:49 +0200

    Initial Commit

---
+++ MultilanguageSwiftExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultilanguageSwiftExample
+//
+//  Created by Dmitry Onishchuk on 01.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
